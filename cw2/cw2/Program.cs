3e846ccf61c3af2be00b13b671e331deb5ae0ad1
Hubert Siwkin <hsiwkin@skydistribution.pl>
2020-11-07 21:11:06 +0100

    Initial project setup

---
+++ cw2/cw2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csvPath, resultPath, format;
+
+            try
+            {
+                csvPath = args[0];
+            } catch
+            {
+                csvPath = "data.csv";
+            }
+
+            try
+            {
+                resultPath = args[1];
+            } catch
+            {
+                resultPath = "result.xml";
+            }
+
+            try
+            {
+                format = args[2];
+            } catch
+            {
+                format = "xml";
+            }
+
+            
+            Console.WriteLine("csvPath: " + csvPath);
+            Console.WriteLine("resultPath: " + resultPath);
+            Console.WriteLine("format: " + format);
+        }
+    }
+}
